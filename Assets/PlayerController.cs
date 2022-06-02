1447e7087f5be491b54bae9629859d7ca124efe8
MaiLinh2407 <linh.hoangngocmai@gmail.com>
2022-06-02 14:10:16 +1000

    Added function to the buttons

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -189,6 +189,6 @@ public class PlayerController : MonoBehaviour
     }
 
     public void Die() {
-        Application.LoadLevel(Application.loadedLevel);
+        SceneManager.LoadScene("Level 1");
     }
 }
